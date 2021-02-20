9de5c92d52f9cbcf29cc2b425a1bbd3882c8a56b
karanbir8872 <wwwkaranbir786@gmail.com>
2021-02-20 16:40:05 +0530

    fix logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -88,7 +88,7 @@ const Footer = ({ home }) => {
             </div>
             <div className="column has-text-centered is-5">
               <LogoImg
-                src="/images/logo.jpg"
+                src="/images/logo.png"
                 alt="footer-logo"
                 className="mb-5"
               />
