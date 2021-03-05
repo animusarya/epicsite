18fee1a2607679f48b41adc966ff1acc0a474f99
karanbir8872 <wwwkaranbir786@gmail.com>
2021-03-05 18:32:17 +0530

    slider fixes

--- src/components/home/Reviews.js
+++ src/components/home/Reviews.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -8,42 +8,22 @@ import Slider from 'react-slick';
 import { Heading, Subtitle } from '../elements';
 
 const Container = styled.div`
-  /* padding-bottom: 8rem; */
   .container {
     background: ${(props) => props.theme.textColor};
     border-radius: 24px;
-  }
-  .slick-dots li button:before {
-    color: ${(props) => props.theme.darkAccent} !important;
-    font-size: 15px;
-    line-height: 20px;
-  }
-
-  .slick-slide {
-    &:focus,
-    a {
-      outline: none !important;
-    }
-  }
-  .slick-list {
-    outline: none !important;
-  }
-  .slick-slide div {
-    outline: none !important;
-  }
-
-  .slick-dots {
-    bottom: -135px;
-  }
-  .slick-dots li.slick-active button:before {
-    color: ${(props) => props.theme.lightAccent} !important;
-    font-size: 10px;
   }
   .column.is-three-fifths {
     margin-right: 2px;
   }
   img {
     border: 2px solid ${(props) => props.theme.backgroundWhite};
+  }
+  .slick-dots {
+    bottom: -115px;
+  }
+  .slick-dots li.slick-active button:before {
+    color: ${(props) => props.theme.lightAccent} !important;
+    font-size: 10px;
   }
 `;
 
@@ -82,65 +62,91 @@ const data = [
   },
 ];
 
-const Reviews = () => {
-  const settings = {
-    dots: true,
-    arrows: false,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    speed: 600,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    lazyLoad: true,
-  };
-  return (
-    <Container className="section is-medium">
-      <div className="container py-6">
-        <div className="columns is-centered">
-          <div className="column is-10">
-            <div className="mb-6 mt-4">
-              <Heading hasWhite centered>
-                What our customer are saying
-              </Heading>
-            </div>
-            <Slider {...settings}>
-              {data.map((item) => (
-                <div key={item.id} className="pb-3 mt-5 mb-6">
-                  <div className="columns">
-                    <div className="column">
-                      <div className="media is-align-items-center">
-                        <div className="media-left">
-                          <figure className="image is-128x128">
-                            <img
-                              className="is-rounded"
-                              src={item.image}
-                              alt="Placeholder"
-                            />
-                          </figure>
+export default class PreviousNextMethods extends Component {
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+
+  next() {
+    this.slider.slickNext();
+  }
+
+  previous() {
+    this.slider.slickPrev();
+  }
+
+  render() {
+    const settings = {
+      arrows: false,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+    return (
+      <Container className="section is-medium">
+        <div className="container py-6">
+          <div className="columns is-centered">
+            <div className="column is-10">
+              <div className="mb-6 mt-4">
+                <Heading hasWhite centered>
+                  What our customer are saying
+                </Heading>
+              </div>
+              <div>
+                <Slider ref={(c) => (this.slider = c)} {...settings}>
+                  {data.map((item) => (
+                    <div key={item.id} className="pb-3 mt-5 mb-6">
+                      <div className="columns">
+                        <div className="column">
+                          <div className="media is-align-items-center">
+                            <div className="media-left">
+                              <figure className="image is-128x128">
+                                <img
+                                  className="is-rounded"
+                                  src={item.image}
+                                  alt="Placeholder"
+                                />
+                              </figure>
+                            </div>
+                            <div className="media-content">
+                              <p className="title is-4 is-family-primary ">
+                                {item.name}
+                              </p>
+                              <p className="subtitle is-6 is-family-primary  is-uppercase">
+                                {item.type}
+                              </p>
+                            </div>
+                          </div>
                         </div>
-                        <div className="media-content">
-                          <p className="title is-4 is-family-primary ">
-                            {item.name}
-                          </p>
-                          <p className="subtitle is-6 is-family-primary  is-uppercase">
-                            {item.type}
-                          </p>
+                        <div className="column ">
+                          <Subtitle hasWhite>{item.review}</Subtitle>
                         </div>
                       </div>
                     </div>
-                    <div className="column ">
-                      <Subtitle hasWhite>{item.review}</Subtitle>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </Slider>
+                  ))}
+                </Slider>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </Container>
-  );
-};
-
-export default Reviews;
+        <div className="columns is-centered mt-5">
+          <div className="column is-1 has-text-right">
+            <button type="button" className="button" onClick={this.previous}>
+              <i className="fas fa-long-arrow-alt-left" />
+            </button>
+          </div>
+          <div className="column is-1" />
+          <div className="column is-1">
+            <button type="button" className="button" onClick={this.next}>
+              <i className="fas fa-long-arrow-alt-right" />
+            </button>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
