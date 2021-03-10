93b42598092c64204137ca06b71652b65de58737
iamkunall <103kunalsharma@gmail.com>
2021-03-10 13:09:25 +0530

    animated button added

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -4,31 +4,24 @@ import styled from 'styled-components';
 const ButtonWrapper = styled.div`
   button {
     background: ${(props) =>
-      props.isTransparent ? 'transparent' : props.theme.backgroundWhite};
+      props.isTransparent ? 'transparent' : props.theme.darkAccent};
     border: 1.5px solid
       ${(props) =>
-        props.borderDark
-          ? props.theme.darkAccent
-          : props.theme.backgroundWhite};
+        props.borderDark ? props.theme.darkAccent : props.theme.darkAccent};
     color: ${(props) =>
       props.isTransparent
         ? props.theme.backgroundWhite
-        : props.theme.textColor};
+        : props.theme.backgroundWhite};
     padding-left: ${(props) => (props.isLarge ? '4rem' : '')};
     padding-right: ${(props) => (props.isLarge ? '4rem' : '')};
-    padding-top: 1.5rem;
-    padding-bottom: 1.5rem;
+    /* padding-top: 1.5rem;
+    padding-bottom: 1.5rem; */
     :hover,
     :focus {
       color: ${(props) =>
         props.isTransparent
           ? props.theme.textColorLite
           : props.theme.textColor} !important;
-      border: 1.5px solid
-        ${(props) =>
-          props.borderDark
-            ? props.theme.darkAccent
-            : props.theme.backgroundWhite};
       box-shadow: none !important;
     }
   }
@@ -38,11 +31,12 @@ const Button = ({ children, ...props }) => {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <ButtonWrapper {...props}>
-      <button
-        type="button"
-        className="button is-size-5  has-text-weight-medium"
-      >
+      <button type="button" className=" animated-link">
         {children}
+        <span />
+        <span />
+        <span />
+        <span />
       </button>
     </ButtonWrapper>
   );
