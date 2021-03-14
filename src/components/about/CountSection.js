24716b07182bd8b61e0e0c98f96da66a0491f16b
karanbir8872 <wwwkaranbir786@gmail.com>
2021-03-14 17:11:40 +0530

    fix the count section

--- src/components/about/CountSection.js
+++ src/components/about/CountSection.js
@@ -1,15 +1,88 @@
 import React from 'react';
 import styled from 'styled-components';
-// import { Subtitle } from '../elements';
+import CountUp from 'react-countup';
+// import { Title } from '../elements';
 
-const Section = styled.div``;
+const ReviewItem = [
+  {
+    id: 1,
+    img: 'https://bulma.io/images/placeholders/1280x960.png',
+    title: 'Used by schools',
+    count: 600,
+    suffix: '+',
+  },
+  {
+    id: 2,
+    img: 'https://bulma.io/images/placeholders/1280x960.png',
+    title: 'Reviews',
+    count: 3000,
+    suffix: '+',
+  },
+  {
+    id: 3,
+    img: 'https://bulma.io/images/placeholders/1280x960.png',
+    title: 'Use our study notes',
+    count: 500,
+    suffix: 'K+',
+  },
+];
+
+const Section = styled.div`
+  background: -webkit-linear-gradient(top, #7a2540 50%, transparent 50%);
+  .card {
+    box-shadow: 0px 10px 20px rgba(41, 41, 42, 0.07);
+    border-radius: 8px;
+  }
+  figure {
+    position: absolute;
+    top: 31%;
+    left: 45.5%;
+    bottom: 0;
+  }
+  .back-round {
+    background: #ffe3c1;
+    height: 64px;
+    width: 64px;
+    border-radius: 100%;
+  }
+  p {
+    font-size: ${(props) => props.theme.fontSizeMedium} !important;
+    color: ${(props) => props.theme.darkShades}!important;
+  }
+  .counting {
+    color: ${(props) => props.theme.mainBrandColor}!important;
+  }
+`;
 
 const CountSection = () => {
   return (
-    <Section className="hero is-medium is-primary ">
-      <div className="hero-body">
-        <div className="container">
-          <h1>hloo</h1>
+    <Section className="section ">
+      <div className="container">
+        <div className="card section">
+          <div className="columns  is-centered">
+            {ReviewItem.map((item) => (
+              <div className="column is-4" key={item.id}>
+                <div className="card-image is-flex is-justify-content-center">
+                  <div className="back-round">
+                    <figure className="image is-32x32">
+                      <img src={item.img} alt="Placeholder-img" />
+                    </figure>
+                  </div>
+                </div>
+                <div className="has-text-centered mt-4">
+                  <CountUp
+                    className="counting is-size-1 has-text-weight-bold"
+                    end={item.count}
+                    duration={2.5}
+                    suffix={item.suffix}
+                  />
+                </div>
+                <div className="content has-text-centered mt-3">
+                  <p className="has-text-weight-medium">{item.title}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </Section>
--- src/components/home/Services.js
+++ src/components/home/Services.js
@@ -66,7 +66,7 @@ const Services = () => {
           </div>
           <div className="columns is-variable is-5 is-multiline is-centered mt-5">
             {ServiceItem.map((item) => (
-              <div className="column is-4">
+              <div key={item.id} className="column is-4">
                 <div className="card p-6 has-radius-medium mb-4">
                   <div className="card-image has-text-centered mb-5 ">
                     <figure className="image is-96x96">
